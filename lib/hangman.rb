1b315d8fd0c202618951d00875b6221fead0fe0f
christiano39 <carneson39@gmail.com>
2019-11-21 14:55:31 -0700

    add save and load functionality

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,11 +1,13 @@
+require 'yaml'
 class Game
-    attr_reader :secret_word, :guessed_letters, :number_of_guesses_remaining, :guessed_word
-    
+    attr_accessor :secret_word, :guessed_letters, :number_of_guesses_remaining, :guessed_word
+    @@save_number = 1
+
     def initialize
         @secret_word = select_good_word
         @guessed_word = secret_word_to_underscores(@secret_word)
         @guessed_letters = []
-        @number_of_guesses_remaining = 12
+        @number_of_guesses_remaining = 8
     end
 
     def display_info
@@ -28,17 +30,17 @@ class Game
         indices = word.each_index.select{|i| word[i] == char}
     end
 
-    def start_game
+    def start_game()
         game_running = true
-        @secret_word = select_good_word
-        @guessed_word = secret_word_to_underscores(@secret_word)
-        @guessed_letters = []
-        @number_of_guesses_remaining = 8
+        
         while game_running
             puts `clear`
             display_info
             
-            user_guess = gets.chomp
+            user_guess = gets.chomp.downcase
+            if user_guess == 'save'
+                save
+            end
             while user_guess.length != 1
                 puts "Please choose a letter"
                 user_guess = gets.chomp
@@ -59,7 +61,7 @@ class Game
             if !@guessed_word.include? '_'
                 puts `clear`
                 puts @guessed_word.join(" ")
-                puts "Nice job! You guessed the word #{@secret_word}!"
+                puts "Nice job! You guessed the word #{@secret_word}"
                 game_running = false
                 play_again
             end
@@ -81,7 +83,58 @@ class Game
             input = gets.chomp.downcase
         end
         if input == 'y'
-            start_game
+            g = Game.new
+            g.start_game
+        end
+    end
+
+    def save()
+        set_savegame_number
+        dump = YAML::dump({:word => @secret_word, :guessed_word => @guessed_word, :guessed_letters => @guessed_letters, :remaining => @number_of_guesses_remaining})
+        filename = "saves/savegame_#{@@save_number}"
+        File.open(filename, 'w') do |f|
+            f << dump
+        end
+        puts "savegame_#{@@save_number} saved!"
+        @@save_number += 1
+    end
+
+    def load(filename)
+        dump = File.read(filename)
+        puts dump
+        game = YAML::load(dump)
+        g = Game.new
+        g.secret_word = game[:word]
+        g.guessed_word = game[:guessed_word]
+        g.guessed_letters = game[:guessed_letters]
+        g.number_of_guesses_remaining = game[:remaining]
+        return g
+    end
+
+    def ask_for_load
+        saves = Dir.entries("saves")
+        saves = saves.select { |save| save.include? "savegame"}
+        saves = saves.sort
+        unless saves.empty?
+            puts "Would you like to load a previously saved game? y/n"
+            input = gets.chomp.downcase
+            while input != 'y' && input != 'n'
+                puts "Please type 'y' for yes or 'n' for no"
+                input = gets.chomp.downcase
+            end
+            if input == 'y'
+                puts saves
+                puts "Please type the name of the save you would like to load"
+                save = gets.chomp.downcase
+                unless saves.include? save
+                    puts "Error: savegame not found"
+                    save = gets.chomp.downcase
+                end
+                filename = "saves/#{save}"
+                load(filename)
+            else
+                return Game.new
+            end
         end
     end
 
@@ -114,10 +167,28 @@ class Game
         new_word = "".rjust(length * 2, "_ ")
         new_word = new_word.split(" ")
     end
+
+    def set_savegame_number
+        saves = Dir.entries("saves")
+        saves = saves.select{ |save| save.include? "savegame" }
+        saves = saves.sort
+        if saves.empty?
+            @@save_number = 1
+        else
+            last_number = saves[-1][-1]
+            @@save_number = last_number.to_i + 1
+        end
+    end
 end
 
 game = Game.new
+game = game.ask_for_load
+if game.nil?
+    game = Game.new
+end
 game.start_game
+
+
 
 
 
